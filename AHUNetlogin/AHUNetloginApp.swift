deb3be28743f0f2a09e8a600b328411d8f01016c
MoeCaa <54182920+MoeclubM@users.noreply.github.com>
2024-11-24 11:02:07 +0800

    Initial Commit

---
+++ AHUNetlogin/AHUNetloginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AHUNetloginApp.swift
+//  AHUNetlogin
+//
+//  Created by Tiancheng Yao on 2024/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AHUNetloginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AHUNetloginTests/AHUNetloginTests.swift
@@ -0,0 +1,16 @@
+//
+//  AHUNetloginTests.swift
+//  AHUNetloginTests
+//
+//  Created by Tiancheng Yao on 2024/11/24.
+//
+
+import Testing
+
+struct AHUNetloginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
